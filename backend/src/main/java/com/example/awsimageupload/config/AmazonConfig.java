90e59b41271ba469f1bea2bfcdded1a3fe5d6b1c
sametaln <a.sametalan56@gmail.com>
2022-05-04 01:12:49 +0300

    initial commit

---
+++ backend/src/main/java/com/example/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,22 @@
+package com.example.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    @Bean
+    public AmazonS3 S3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIAVJVJREX4H3PHXYWE","X7VhYIsvMR2vX2iRod6Y6oHyAho/0DyXdP9g2jJ1"
+        );
+
+        return AmazonS3ClientBuilder.standard().withRegion("eu-central-1").withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+    }
+}
+
---
+++ backend/src/main/java/com/example/awsimageupload/profile/UserProfileService.java
@@ -0,0 +1,28 @@
+package com.example.awsimageupload.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserProfileService {
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // Check if image is not empty.
+        // If file is an image and user exists in database then grab some metadata from file if any
+        // and save it to S3 and update database with image url.
+    }
+}
